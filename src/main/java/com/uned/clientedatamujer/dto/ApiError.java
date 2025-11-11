1472b1fb4212c0ef1ca4ee94c804caad4bc3801b
glunah2001 <glunah2001@gmail.com>
2025-11-10 19:01:24 -0600

    Corrección en la deserialización de errores emitidos desde la API. Se ha corregido un error persistente desde la API. Se han agregado otros indicadores de segundo en el timestamp del error.

--- src/main/java/com/uned/clientedatamujer/dto/ApiError.java
+++ src/main/java/com/uned/clientedatamujer/dto/ApiError.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record ApiError(
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS") LocalDateTime timestamp,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS") LocalDateTime timestamp,
         int status,
         String error,
         String message,
